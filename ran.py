81ad9c5acfa4dc512083a4db357059b599b03106
knox <100846845+Encryptedknox@users.noreply.github.com>
2023-05-15 13:49:38 +0530

    Add files via upload

---
+++ ran.py
@@ -0,0 +1,24 @@
+import telebot
+import string
+from random import choice
+
+# Create an instance of the TeleBot class
+bot = telebot.TeleBot("6138107355:AAGuP7Rtf36n8NQGNwPehnn5ClTGBtZeEeQ")
+
+# Define a message handler that handles incoming /start command
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, f"Hello, {message.chat.username}")
+
+# Define another message handler that handles /generate_password command
+@bot.message_handler(commands=['generate_password'])
+def generate_password(message):
+    # Generate a random password
+    characters = string.ascii_letters + string.punctuation + string.digits
+    password = "".join(choice(characters) for _ in range(random.randint(8, 16)))
+
+    # Send the password to the user
+    bot.send_message(message.chat.id, f"Generated password: {password}")
+
+# Run the bot
+bot.polling()
